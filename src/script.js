06a167f4e6c528f7c6796e85ac5353091b9bab43
JOHN MUGENDI KIMOTHO <149392018+CGAJAY@users.noreply.github.com>
2024-03-23 10:12:49 +0000

    Commenting on the JS code

--- src/script.js
+++ src/script.js
@@ -1,19 +1,22 @@
 let results;
 let problem = '';
 let display = document.querySelector(".display");
-
 display.innerHTML = 0;
+
+// This function only takes the calculation to be done
 function calculation(num){
     problem += num;
     display.innerHTML = problem;
 };
 
+// This function takes the calculation and evaluates it
 function getAnswer(){
     answer = eval(problem);
     display.innerHTML = answer;
     problem = answer;
 };
 
+// This function resets all the values back to zero
 function reset() {
     results = 0;
     problem = ''; 
